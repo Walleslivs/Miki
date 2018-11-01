d70a6856ce9e7f908314a17007c989e3dbc7e0c9
johancesar <johancesar50@gmail.com>
2018-11-01 11:41:44 +0100

    Animation

--- app/src/main/java/com/example/walleslivs/miki/MainActivity.kt
+++ app/src/main/java/com/example/walleslivs/miki/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.walleslivs.miki
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
- //shooo
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+//shooo
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        ko.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.bounce)
+            ko.startAnimation(animation)
+        }
     }
 }
+
